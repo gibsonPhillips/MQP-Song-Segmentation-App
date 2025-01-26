bf96ec30836ad61aa68614454b44e4809fb3e764
Seth Frank <sbfrank@wpi.edu>
2025-01-25 19:16:32 -0500

    Move some files around

--- Scripts/page.js
+++ Scripts/page.js
@@ -1,4 +1,60 @@
+import WaveSurfer from 'https://unpkg.com/wavesurfer.js@7.8.16/dist/wavesurfer.esm.js';
+import RegionsPlugin from 'https://unpkg.com/wavesurfer.js@7.8.16/dist/plugins/regions.esm.js';
+import ZoomPlugin from 'https://unpkg.com/wavesurfer.js@7.8.16/dist/plugins/zoom.esm.js';
+
+
 let filePath = ''
+let minPxPerSec = 100
+let colorMap = new Map();
+
+// Initialize the Regions plugin
+const regions = RegionsPlugin.create()
+
+const zoom = ZoomPlugin.create({
+    // the amount of zoom per wheel step, e.g. 0.5 means a 50% magnification per scroll
+    scale: 0.1,
+  });
+
+// Create an instance of WaveSurfer
+const wavesurfer = WaveSurfer.create({
+    container: '#waveform',
+    waveColor: 'rgb(200, 0, 200)',
+    progressColor: 'rgb(100, 0, 100)',
+    minPxPerSec: 100,
+    plugins: [regions, zoom],
+})
+
+// Give regions a random color when they are created
+const random = (min, max) => Math.random() * (max - min) + min
+const randomColor = () => `rgba(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)}, 0.5)`
+
+const playButton = document.querySelector('#play')
+const forwardButton = document.querySelector('#forward')
+const backButton = document.querySelector('#backward')
+const zoomInButton = document.querySelector('#zoom-in')
+const zoomOutButton = document.querySelector('#zoom-out')
+
+playButton.onclick = () => {
+    wavesurfer.playPause()
+}
+
+forwardButton.onclick = () => {
+    wavesurfer.skip(5)
+}
+
+backButton.onclick = () => {
+    wavesurfer.skip(-5)
+}
+
+zoomInButton.onclick = () => {
+    minPxPerSec = minPxPerSec + 10
+    wavesurfer.zoom(minPxPerSec)
+}
+
+zoomOutButton.onclick = () => {
+    minPxPerSec = minPxPerSec - 10
+    wavesurfer.zoom(minPxPerSec)
+}
 
 
 document.getElementById("segment-algorithm1").addEventListener("click", () => {segment(1)});
@@ -13,6 +69,8 @@ document.getElementById('chooseSong').addEventListener('click', async () => {
         document.getElementById('filePath').textContent = `Selected file: ${filePaths[0]}`;
         console.log('File path:', filePaths[0]); // This is available in Electron or environments with full file access
         filePath = filePaths[0];
+        regions.clearRegions();
+        wavesurfer.load(filePaths[0]);
     } else {
         document.getElementById('filePath').textContent = 'No file selected.';
         console.log('No file selected');
@@ -49,6 +107,8 @@ async function segment(algorithm) {
 function updateSegmentElementsList(elements) {
     const tbody = document.getElementById('segment-elements');
     tbody.innerHTML = ''
+    regions.clearRegions()
+    colorMap.clear();
     elements.forEach(element => {
         let tr = document.createElement('tr');
         element.forEach(item => {
@@ -57,5 +117,55 @@ function updateSegmentElementsList(elements) {
             tr.appendChild(td)
         });
         tbody.appendChild(tr);
+
+        if(!colorMap.has(element[3])) {
+            colorMap.set(element[3], randomColor());
+        }
+
+        regions.addRegion({
+            start: element[1],
+            end: element[2],
+            content: 'Section ' + element[3],
+            color: colorMap.get(element[3]),
+            drag: false,
+            resize: false,
+        })
     });
 }
+
+
+
+
+
+
+
+
+
+
+// regions.enableDragSelection({
+//     color: 'rgba(255, 0, 0, 0.1)',
+// })
+
+// // Add event listener for region creation
+// regions.on('region-created', (region) => {
+//     console.log("REGION CREATED");
+//     zoomToRegion(region.start, region.end);
+//     region.remove();
+// });
+
+// // Function to zoom into a specific region
+// function zoomToRegion(start, end) {
+//     const totalDuration = wavesurfer.getDuration();
+//     const regionDuration = end - start;
+
+//     // Calculate zoom level and center
+//     const zoomLevel = totalDuration / regionDuration;
+//     const center = (start + end) / 2;
+
+//     // Set zoom level and scroll to the region
+//     wavesurfer.zoom(zoomLevel);
+
+//     // const pxPerSec = wavesurfer.minPxPerSec * zoomLevel;
+//     // const scrollPosition = Math.max(center * pxPerSec - wavesurfer.container.clientWidth / 2, 0);
+//     // wavesurfer.drawer.wrapper.scrollLeft = scrollPosition;
+// }
