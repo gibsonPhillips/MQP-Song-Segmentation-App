63e218461090bc0700a6bd320c21373e4c512f70
Vincent Boothroyd <128171255+vboothroyd@users.noreply.github.com>
2025-02-05 21:52:23 -0500

    Revert "Added init project-saving.js to organize"
    This reverts commit 2d6d87be660d550c41806fcb4795cd60ca50d2c4.

--- Scripts/page.js
+++ Scripts/page.js
@@ -10,8 +10,26 @@ let colorMap = new Map();
 
 
 // Sort out the save file system
-let workspace = getWorkspace()
+let workspace = ''
 
+let appdataPromise = window.api.getAppData().then((appdata) => {
+    console.log(appdata);
+    workspace = appdata + '\\Song Segmentation'
+    console.log(workspace)
+
+    // Create a directory if it doesnt already exist
+    window.api.createDirectory(workspace).then((result) => {
+
+        console.log('Directory handled successfully.');
+
+    }).catch((error) => {
+        // Throw error if there is an issue creating the directory
+        console.error(error);
+    });
+}).catch((error) => {
+    // Throw error if there is an issue getting the appdata environment variable
+    console.error(error);
+});
 var data
 
 // Initialize the Regions plugin
