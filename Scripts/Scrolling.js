ba18bd9f90054b9d4b5bc327abbcb328bb5c8040
Gibosn <gcphillips@gmail.com>
2025-02-24 16:33:42 -0500

    restructured the windows to look good and implimented full scrolling link

--- Scripts/Scrolling.js
+++ Scripts/Scrolling.js
@@ -1,17 +1,20 @@
 // Sync scrolling between two separated scroll panes
-const Tracks = document.getElementById('tracks');
-const Waveforms = document.getElementById('waveforms');
+const tracks = document.getElementById('tracks');
+const timeline = document.getElementById('timeline');
 
 let isSyncingScroll = false;
 
 // this flashes the functionality so scrolling 
 // isn't detected as a scroll action to sync
 function syncScroll(source, target) {
+    console.log("here with ", source, " and ", target)
     if (isSyncingScroll) return;
     isSyncingScroll = true;
     target.scrollTop = source.scrollTop;
     isSyncingScroll = false;
 }
 
-Tracks.addEventListener('scroll', () => syncScroll(Tracks, Waveforms));
-Waveforms.addEventListener('scroll', () => syncScroll(Waveforms, Tracks));
+tracks.addEventListener('scroll', () => syncScroll(tracks, timeline));
+timeline.addEventListener('scroll', () => syncScroll(timeline, tracks));
+
+
